467b6b0ff1899c01c3b874d9fca6167838e2101a
ahmad <ahmadnahal7@gmail.com>
2025-03-14 16:40:08 +0100

    final update >> edit

--- tickets/serializer.py
+++ tickets/serializer.py
@@ -7,31 +7,28 @@ from .models import Movie, Showtime
 class ShowtimeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Showtime
-        fields = ['id', 'date', 'time', 'hall', 'total_seats', 'available_seats', 'ticket_price', 'reserved_seats']
-
+        fields = ['date', 'time', 'hall', 'total_seats', 'available_seats', 'ticket_price', 'reserved_seats']
 
 class MovieSerializer(serializers.ModelSerializer):
-    # استدعاء الـ ShowtimeSerializer لتضمين تفاصيل الشو تايم
-    show_times = ShowtimeSerializer(many=True, read_only=True)  # عرض تفاصيل الشو تايم (العروض المرتبطة بالفيلم)
+    show_times = ShowtimeSerializer(many=True)  # السماح بإرسال أوقات العرض مع الفيلم
 
     class Meta:
         model = Movie
         fields = [
-            'id',
-            'name',
-            'show_times',  # تضمين العروض المرتبطة بالفيلم
-            'photo',
-            'vertical_photo',
-            'description',
-            'short_description',
-            'sponsor_video',
-            'actors',
-            'release_date',
-            'added_date',
-            'duration',
-            'imdb_rating',
-            'tags'
+            'id', 'name', 'photo', 'vertical_photo', 'description', 'short_description',
+            'sponsor_video', 'actors', 'release_date', 'added_date', 'duration',
+            'imdb_rating', 'tags', 'show_times'
         ]
+
+    def create(self, validated_data):
+        """إنشاء فيلم مع أوقات العرض"""
+        show_times_data = validated_data.pop('show_times', [])  # استخراج أوقات العرض من البيانات
+        movie = Movie.objects.create(**validated_data)
+
+        for showtime_data in show_times_data:
+            Showtime.objects.create(movie=movie, **showtime_data)  # إنشاء أوقات العرض وربطها بالفيلم
+
+        return movie
 
 
 class GuestSerializer(serializers.ModelSerializer):
